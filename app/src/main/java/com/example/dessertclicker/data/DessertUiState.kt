d2c732987b1e729d476deada31d47fba4aa53b2a
romero mendes <romeromende@gmail.com>
2023-07-04 09:30:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/dessertclicker/data/DessertUiState.kt
@@ -0,0 +1,16 @@
+package com.example.dessertclicker.data
+
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import com.example.dessertclicker.data.Datasource.dessertList
+
+data class DessertUiState(
+    val revenue: Int = 0,
+    val dessertsSold: Int = 0,
+    val currentDessertIndex: Int = 0,
+    val currentDessertPrice: Int = dessertList[currentDessertIndex].price,
+    @DrawableRes val currentDessertImageId: Int = dessertList[currentDessertIndex].imageId
+)
